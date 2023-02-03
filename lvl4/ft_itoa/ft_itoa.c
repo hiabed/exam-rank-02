0649832148cb5571e9b9d180a6250133a448d423
Abed <abed.hassani.idrissi@gmail.com>
2023-02-03 18:49:44 +0100

    mhassani

---
+++ lvl4/ft_itoa/ft_itoa.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int num_len(int n)
+{
+    int i = 0;
+    if(n <= 0)
+    {
+        n = n * -1;
+        i++;
+    }
+    while(n >= 1)
+    {
+        i++;
+        n /= 10;
+    }
+    return i;
+}
+
+int power_of_ten(int n)
+{
+    int i = 0;
+    int d = 1;
+    while(i < n)
+    {
+        d = d * 10;
+        i++;
+    }
+    return d;
+
+}
+
+char	*ft_itoa(int nbr)
+{
+    int i = 0;
+    int len = num_len(nbr);
+    int len_condition = num_len(nbr);
+    char *s = malloc((num_len(nbr) * sizeof(char)) + 1);
+    if(nbr < 0)
+    {
+        nbr = nbr * -1;
+        s[i] = '-';
+        i++;
+        while(i < len_condition)
+        {        //3  / 1 ==> 3 + 48 = '2';
+            s[i] = (nbr / power_of_ten(len - 2)) + '0';
+            nbr = nbr % power_of_ten(len - 2);    //23 % 10 => 23;
+            len--;
+            i++;
+        }
+    }
+    while(i < len_condition)
+    {
+        s[i] = (nbr / power_of_ten(len - 1)) + '0';
+        nbr = nbr % power_of_ten(len - 1);
+        len--;
+        i++;
+    }
+    return s;
+}
+
+// int main()
+// {
+//     printf("ft_itoa => %s\n", ft_itoa(2));
+//     printf("10 pow of 2: %d\n", power_of_ten(2));
+//     printf("num len => %d", num_len(0));
+//     return 0;
+// }
